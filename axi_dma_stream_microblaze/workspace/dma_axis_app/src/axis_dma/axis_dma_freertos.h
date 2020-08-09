542b2aa224dd571033b3323ceb334a3de36e08a7
Tim Michals <tcmichals@yahoo.com>
2020-08-09 13:26:12 -0500

    removed projects

--- axi_dma_stream_microblaze/workspace/dma_axis_app/src/axis_dma/axis_dma_freertos.h
+++
@@ -1,7 +0,0 @@
-
-
-bool setupDMA(void);
-bool freeRxPkt(uint8_t *rxPkt);
-bool wait_rx_pkt( uint8_t * &rxPkt, size_t &lenOfPkt,TickType_t xTicksToWait );
-bool wait_tx_pkt( uint8_t *txPkt, size_t lenOfPkt);
-bool allocTx(uint8_t *&txPkt, size_t &lenOfTxPkt);
